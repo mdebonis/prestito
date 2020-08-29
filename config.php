2e9a5dae95d722ceadca03be60d754908731752f
Michelangelo De Bonis <perfectprogr@gmail.com>
2020-08-29 20:26:15 +0200

    Second update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$host = $ENV["APP_HOST"];
-$user = $ENV["APP_DB_USER"];
-$password = $ENV["APP_DB_PASSWORD"];
-$database = $ENV["APP_DB_NAME"];
+$host = $_ENV["APP_HOST"];
+$user = $_ENV["APP_DB_USER"];
+$password = $_ENV["APP_DB_PASSWORD"];
+$database = $_ENV["APP_DB_NAME"];
 ?>

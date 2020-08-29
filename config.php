25f58feb79de8cdae00aa4d54d106a548cc81129
Michelangelo De Bonis <perfectprogr@gmail.com>
2020-08-29 19:05:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$host ='localhost';
-$user ='root';
-$password = '';
-$database = 'prestito';
+$host = $ENV["APP_HOST"];
+$user = $ENV["APP_DB_USER"];
+$password = $ENV["APP_DB_PASSWORD"];
+$database = $ENV["APP_DB_NAME"];
 ?>
